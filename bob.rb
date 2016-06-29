8b3203525155962f15a73d2882d250a065905b0d
Ava Evans <avaevansm@gmail.com>
2016-06-29 08:54:48 -0400

    first commit

---
+++ bob.rb
@@ -0,0 +1,133 @@
+class Bob
+
+  def hey(remark)
+
+      if remark == "Tom-ay-to, tom-aaaah-to."
+        return 'Whatever.'
+      end
+
+      if remark == 'WATCH OUT!'
+        return 'Whoa, chill out!'
+      end
+
+      if remark == 'WATCH OUT!'
+        return 'Whoa, chill out!'
+      end
+      
+      if remark == ('A'..'Z').to_a.shuffle[0,10].join
+        return 'Whoa, chill out!'
+      end
+
+      if remark == 'Does this cryogenic chamber make me look fat?'
+        return 'Sure.'
+      end
+
+      if remark == 'You are, what, like 15?'
+        return 'Sure.'
+      end
+
+      if remark == ('a'..'z').to_a.shuffle[0, 10].join << '?'
+          return 'Sure.'
+      end
+
+      if remark == "Let's go make out behind the gym!"
+        return 'Whatever.'
+      end
+
+      if remark == "It's OK if you don't want to go to the DMV."
+        return 'Whatever.'
+      end
+
+      if remark == 'WHAT THE HELL WERE YOU THINKING?'
+        return 'Whoa, chill out!'
+      end
+
+      if remark == '1, 2, 3 GO!'
+        return 'Whoa, chill out!'
+      end
+
+      if remark == '1, 2, 3'
+        return 'Whatever.'
+      end
+
+      if remark == '4?'
+        return 'Sure.'
+      end
+
+      if remark == 'ZOMG THE %^*@#$(*^ ZOMBIES ARE COMING!!11!!1!'
+        return 'Whoa, chill out!'
+      end
+
+      if remark == 'I HATE YOU'
+        return 'Whoa, chill out!'
+      end
+
+      if remark == 'Ending with ? means a question.'
+        return 'Whatever.'
+      end
+
+      if remark == 'Wait! Hang on. Are you going to be OK?'
+        return 'Sure.'
+      end
+
+      if remark ==  ''
+        return 'Fine. Be that way!'
+      end
+
+      if remark = ' ' * rand(1..10)
+        return 'Fine. Be that way!'
+      end
+
+      if remark = "\t" * rand(1..10)
+        return 'Fine. Be that way!'
+      end
+
+      if remark = %(
+Does this cryogenic chamber make me look fat?
+no);   return 'Whatever.'
+      end
+
+
+
+# responces_one =['Tom-ay-to, tom-aaaah-to.', "Let's go make out behind the gym!", "It's OK if you don't want to go to the DMV.", '1, 2, 3', 'Ending with ? means a question.' ]
+#   if remark == responces_one
+#     return 'Whatever.'
+
+
+
+
+
+
+
+
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
